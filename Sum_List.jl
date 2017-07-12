e0b944f703cf49f61a96fb2ae45c63a49f8c9a30
Lynn <yeji.park.yeji@gmail.com>
2017-07-12 16:18:32 +0900

    make function

---
+++ Sum_List.jl
@@ -0,0 +1,41 @@
+List = [1:1e+06...]
+
+# t = 0
+# for j = 1:10
+#     start=time()
+#     variable = 0
+#     for i in List
+#         variable = variable + i 
+#     end
+#     elapsed=time()-start
+#     t += elapsed
+# end
+
+function Sum()
+    start = time()
+    s = 0
+    for i in List
+        s += i
+    end
+    elapsed = time() - start
+    return s, elapsed
+end
+
+s, _ = Sum()
+
+t = 0
+
+for j=1:10
+    _,elapsed = Sum()
+    t += elapsed
+end
+
+println(s)
+println("Time : ", t/10, " (10 times)")
+
+start_2 = time()
+A = sum(List)
+elapsed_2 = time()-start_2
+println(A)
+println("Time_2 : ", elapsed_2)
+
